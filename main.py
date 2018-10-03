ffc7ddba3fe848151a74dd601bd1a69318b086bf
CyberpunkEngineer <j.einerhand@live.nl>
2018-10-03 22:46:04 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -194,6 +194,7 @@ def main():
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                pygame.display.quit()
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_q:
